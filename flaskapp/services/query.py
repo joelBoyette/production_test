3735f67368dfb7b9497a0f7c7da56f8aeeda9525
Joel Boyette <joelboyette@gmail.com>
2020-10-15 17:23:57 -0400

    update

--- flaskapp/services/query.py
+++ flaskapp/services/query.py
@@ -2,22 +2,22 @@ import pyodbc
 import pandas as pd
 
 
-def get_data():
-
-    # CONNECT TO ACCESS DB
-    conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};'
-                          r'DBQ=/apps/app_repo/flaskapp/production_db.accdb'
-                          )
-
-                          # r'DBQ=C:\Users\joelb\Documents\Development\production_test\flaskapp\production_db.accdb;')
-
-    # BASE SQL
-    sql = f"""SELECT *
-              FROM person
-          """
-    # CONVERT SQL TO PANDAS
-    person_df = pd.read_sql(sql, conn)
-
-    return person_df
+# def get_data():
+#
+#     # CONNECT TO ACCESS DB
+#     conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};'
+#                           r'DBQ=/apps/app_repo/flaskapp/production_db.accdb'
+#                           )
+#
+#                           # r'DBQ=C:\Users\joelb\Documents\Development\production_test\flaskapp\production_db.accdb;')
+#
+#     # BASE SQL
+#     sql = f"""SELECT *
+#               FROM person
+#           """
+#     # CONVERT SQL TO PANDAS
+#     person_df = pd.read_sql(sql, conn)
+#
+#     return person_df
 
 
--- flaskapp/views/test_views.py
+++ flaskapp/views/test_views.py
@@ -1,6 +1,6 @@
 import flask
 from flaskapp.infrastructure.view_modifiers import response
-from flaskapp.services.query import get_data
+# from flaskapp.services.query import get_data
 
 blueprint = flask.Blueprint('index', __name__, template_folder='templates')
 
@@ -11,7 +11,9 @@ blueprint = flask.Blueprint('index', __name__, template_folder='templates')
 @response(template_file='index.html')
 def index():
 
-    data = get_data()
-    data_dict = data.to_dict()
+    # data = get_data()
+    # data_dict = data.to_dict()
+    #
+    # return {'data': data_dict}
 
-    return {'data': data_dict}
+    return {}
