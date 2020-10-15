48c02b76a3e5aaaf1cff647ea230253b42ac20f0
Joel Boyette <joelboyette@gmail.com>
2020-10-15 17:17:38 -0400

    try db github connection

--- flaskapp/services/query.py
+++ flaskapp/services/query.py
@@ -6,7 +6,10 @@ def get_data():
 
     # CONNECT TO ACCESS DB
     conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};'
-                          r'DBQ=C:\Users\joelb\Documents\Development\production_test\flaskapp\production_db.accdb;')
+                          r'DBQ=/apps/app_repo/flaskapp/production_db.accdb'
+                          )
+
+                          # r'DBQ=C:\Users\joelb\Documents\Development\production_test\flaskapp\production_db.accdb;')
 
     # BASE SQL
     sql = f"""SELECT *
