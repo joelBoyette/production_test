858104b1d44c51491b35aed5e7ce0c207b0caaf7
Joel Boyette <joelboyette@gmail.com>
2020-10-16 11:50:21 -0400

    testing

--- flaskapp/services/query.py
+++ flaskapp/services/query.py
@@ -6,7 +6,7 @@ def get_data():
 
     # CONNECT TO ACCESS DB
     conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};'
-                          r'DBQ=/apps/app_repo/flaskapp/production_db.accdb'
+                          'DBQ=/apps/app_repo/flaskapp/production_db.accdb'
                           )
 
     # BASE SQL
